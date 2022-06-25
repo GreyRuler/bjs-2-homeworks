7d7d52b8d5ae330e0ede5d7f058b129d4476195e
GreyRuler <daniil_vasilenko757@mail.ru>
2022-06-25 18:38:41 +0300

    first commit variables

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,29 @@
+"use strict";
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let d = b**2-4*a*c;
+  if (d < 0) return [];
+  else if (d === 0) return [-b/(2*a)]
+  else return [(-b + Math.sqrt(d) )/(2*a), (-b - Math.sqrt(d) )/(2*a)];
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
+
+  if (isNaN(parseInt(percent))) {
+    return `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`
+  } else if (isNaN(parseInt(contribution))) {
+    return `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`
+  } else if (isNaN(parseInt(amount))) {
+    return `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`
+  }
+  percent = parseInt(percent);
+  contribution = parseInt(contribution);
+  amount = parseInt(amount);
+
   let totalAmount;
-
-  // код для задачи №2 писать здесь
-
+  let S = amount - contribution;
+  let P = percent/12/100;
+  let n = Math.floor((date - Date.now())/1000/60/60/24/30);
+  totalAmount = +((S * (P + (P / (((1 + P)**n) - 1))) * n).toFixed(2));
+  console.log(totalAmount);
   return totalAmount;
 }
