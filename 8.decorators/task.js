1ade0dd2dd64d7af2002daa0df22ac143714fb87
GreyRuler <daniil_vasilenko757@mail.ru>
2022-07-13 21:53:08 +0300

    Impl

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,55 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = {};
+
+  function add(...args) {
+    const hash = args.toString();
+    if (hash in cache) {
+      console.log("Из кэша: " + cache[hash]);
+      return "Из кэша: " + cache[hash];
+    } else {
+      if (cache.length > 5) {
+        delete cache[0];
+      }
+      let result = func(...args);
+      cache[hash] = result;
+      console.log("Вычисляем: " + result);
+      return "Вычисляем: " + result;
+    }
+  }
+
+  return add;
 }
 
+function debounceDecoratorNew(func, ms) {
+  let flag = false;
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+  return function () {
+    if (flag) {
+      return;
+    }
+    flag = true;
+    setTimeout(() => {
+      flag = false;
+      func();
+    }, ms);
+  };
 }
 
-function debounceDecorator2(func) {
-  // Ваш код
+function debounceDecorator2(func, ms) {
+  let flag = false;
+
+  function wrapper() {
+    wrapper.count += 1;
+    if (flag) {
+      return;
+    }
+    flag = true;
+    setTimeout(() => {
+      flag = false;
+      func();
+    }, ms);
+  };
+
+  wrapper.count = 0;
+  return wrapper;
 }
